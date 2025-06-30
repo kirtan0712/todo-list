b5df381b00d311f6c89a82a9e5a7bdd88b794e2f
kirtan0712 <kirtanbrahmbhatt@fxis.ai>
2025-06-30 12:00:38 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+  function addTask() {
+    if (task.trim() === '') return;
+    setTaskList([...taskList, task]);
+    setTask('');
+  }
+  function deleteTask(indexToRemove) {
+    const updatedList = taskList.filter((_, index) => index !== indexToRemove);
+    setTaskList(updatedList);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <h1>Todo List</h1>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            placeholder="Enter your Task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button onClick={addTask}>Add</button>
+        </div>
+        <div className="task-list">
+          {taskList.map((t, index) => (
+            <div key={index} className="task-item">
+              <span>{t}</span>
+              <button className="delete-btn" onClick={() => deleteTask(index)}>❌</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
